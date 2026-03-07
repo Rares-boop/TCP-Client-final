023569b301da6b33c1c6571995a561ec89e9f522
Rares <vladurares@gmail.com>
2026-03-07 19:11:50 +0200

    fixed logout already bug

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -32,23 +32,9 @@ android {
 }
 
 android {
-    // ... compileSdk, defaultConfig, etc ...
-
-    // === BAGA BLOCUL ASTA AICI ===
     packaging {
         resources {
-            // Ii zicem sa ignore fisierul care face scandal
             excludes += "/META-INF/versions/9/OSGI-INF/MANIFEST.MF"
-
-            // Daca mai ai erori cu LICENSE sau NOTICE, decomenteaza astea:
-            // excludes += "META-INF/DEPENDENCIES"
-            // excludes += "META-INF/LICENSE"
-            // excludes += "META-INF/LICENSE.txt"
-            // excludes += "META-INF/license.txt"
-            // excludes += "META-INF/NOTICE"
-            // excludes += "META-INF/NOTICE.txt"
-            // excludes += "META-INF/notice.txt"
-            // excludes += "META-INF/ASL2.0"
         }
     }
 }
@@ -59,7 +45,6 @@ dependencies {
     implementation(libs.material)
     implementation(libs.activity)
     implementation(libs.constraintlayout)
-    //implementation(files("C:/Users/Rares/Desktop/TCPClient/app/libs/ChatClasses.jar "))
     implementation(files("libs/chat-lib.jar"))
     implementation("androidx.security:security-crypto:1.1.0-alpha06")
     testImplementation(libs.junit)
@@ -71,18 +56,6 @@ dependencies {
     implementation(files("libs/bcprov-jdk18on-1.83.jar"))
     implementation(files("libs/bcpkix-jdk18on-1.83.jar"))
 
-    implementation("androidx.room:room-runtime:2.6.1")
-    annotationProcessor("androidx.room:room-compiler:2.6.1")
-
-    // === SQLCIPHER (Criptare baza de date) ===
-    // Asta face ca Room sa fie "Seif". Fara asta, oricine cu root vede cheile.
-    implementation("net.zetetic:android-database-sqlcipher:4.5.4")
-
-    // Fix pentru SQLCipher pe Android modern
-    implementation("androidx.sqlite:sqlite:2.4.0")
-    implementation("androidx.security:security-crypto:1.1.0-alpha06")
-
-    // Astea îți lipsesc, de-aia crapă camera la start!
     implementation("androidx.camera:camera-core:1.3.1")
     implementation("androidx.camera:camera-camera2:1.3.1")
     implementation("androidx.camera:camera-lifecycle:1.3.1")
